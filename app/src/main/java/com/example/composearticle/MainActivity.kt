f1bd645d33c78e0ea6aa613f6c164e073ada2b68
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-04-17 20:19:06 +0530

    commit

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.composearticle
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
                 }
             }
         }
@@ -30,14 +34,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+fun ArticleTitleImg(titleName:String,articleDesc:String){
+    val titleImage = painterResource(id = R.drawable.bg_compose_background)
+    Box() {
+        Image(painter = titleImage, contentDescription = null)
+    }
+    Box() {
+        Text(
+            text = titleName
+
+        ) //Title
+        Text(text = articleDesc)
+    }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeArticleTheme {
-        Greeting("Android")
+        ArticleTitleImg("Jetpack Compose Tutorial","lets gooo baby")
     }
 }
